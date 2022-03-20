8c4a694de9f18fa390459869e14af4433102f234
Stevewu1234 <plgawu@163.com>
2022-03-20 18:40:33 +0800

    update task function

--- scripts/tasks/test.ts
+++ scripts/tasks/test.ts
@@ -3,20 +3,22 @@ import { task } from 'hardhat/config';
 import * as config from '../config';
 import { getABI, readDeployedContractDetails } from '../utils';
 
-task('test-storage', 'just to find contract storage data').setAction(
-  async (args, hre) => {
-    // data preparation
-    const contractname = config.contractDetails.StorageTest.name;
-    const address = readDeployedContractDetails(contractname).address;
-    const abi = getABI(contractname);
-    const provider = hre.ethers.provider;
-    const contract = await new hre.ethers.Contract(address, abi, provider);
+task('test', 'just to find contract storage data')
+  .addParam('action', 'select testing function')
+  .setAction(async (args, hre) => {
+    if (args === 'storage') {
+      // data preparation
+      const contractname = config.contractDetails.StorageTest.name;
+      const address = readDeployedContractDetails(contractname).address;
+      const abi = getABI(contractname);
+      const provider = hre.ethers.provider;
+      const contract = await new hre.ethers.Contract(address, abi, provider);
 
-    // data reading
-    const storageData = await hre.ethers.provider.getStorageAt(address, 2);
-    const aValue = await contract.callStatic.a();
+      // data reading
+      const storageData = await hre.ethers.provider.getStorageAt(address, 2);
+      const aValue = await contract.callStatic.a();
 
-    console.log(aValue);
-    console.log(storageData);
-  }
-);
+      console.log(aValue);
+      console.log(storageData);
+    }
+  });
