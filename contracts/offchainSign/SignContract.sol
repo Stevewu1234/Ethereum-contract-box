c4c0d5ee04895a6ae5150b809221c79f0997c483
Stevewu1234 <plgawu@163.com>
2022-07-23 18:11:06 +0800

    update sign contract test logic with two sign function

--- contracts/offchainSign/SignContract.sol
+++ contracts/offchainSign/SignContract.sol
@@ -4,37 +4,45 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-// todo: check if the msg.sender of the signed message sent by other address is equal to the signer
-// todo: check if the signed message can be used by address to operate signer's value
-
-contract SignContract is EIP712, Ownable {
+contract SignTestContract is EIP712, Ownable {
     address public Permit1RecordUser;
     address public Permit2RecordUser;
 
+    string public testString;
+
     // avoid to multiply call the same func
-    mapping (address => uint) public nonces;
+    mapping (address => uint) public nonce;
 
-    bytes32 public constant PERMIT_TYPEHASH = keccak256("PermitWithValue(address owner,uint256 value,uint256 deadline)");
+    bytes32 public constant PERMIT_TYPEHASH = keccak256("PermitWithValue(address owner,uint256 value,string testString,uint256 deadline,uint256 nonce)");
 
-    constructor() EIP712("SignContract", "1") {}
+    constructor() EIP712("SignTestContract", "1") {}
 
     // generate a hash message to verify the generated offchain signature
     function PermitWithValue(        
         address owner, 
         uint256 value,
+        string memory _testString,
         uint256 deadline,
         bytes memory signature
         ) public {
 
-        bytes32 structHash = keccak256(abi.encode(PERMIT_TYPEHASH, owner, value, deadline));
+        bytes32 structHash = keccak256(abi.encode(
+            PERMIT_TYPEHASH, 
+            owner,
+            value,
+            keccak256(bytes(_testString)),
+            deadline, 
+            nonce[owner]++
+        ));
         bytes32 messageHash = _hashTypedDataV4(structHash);
 
         address signer = ECDSA.recover(messageHash, signature);
-        require(signer == owner, "invalid data");
+        require(signer == owner, "invalid signature");
 
-        Permit1RecordUser = _msgSender();
+        Permit1RecordUser = owner;
+        testString = _testString;
 
-        emit PermittedWithValue(owner, value, deadline);
+        emit PermittedWithValue(owner, value, testString, deadline);
     }
 
     // verify the generated offchain signature directly
@@ -43,14 +51,32 @@ contract SignContract is EIP712, Ownable {
         bytes memory signature
         ) public {
         address signer = ECDSA.recover(message, signature);
-        require(signer == msg.sender, "invalid data");
+        require(signer == msg.sender, "invalid signature");
 
         Permit2RecordUser = _msgSender();
 
         emit PermittedWithHashedMessage(signer);
     }
 
+    function testSignatureMessage(
+        address owner, 
+        uint256 value,
+        string memory _testString,
+        uint256 deadline
+    ) external view returns (bytes32 domainHash, bytes32 structHash, bytes32 messageHash) {
+        domainHash = _domainSeparatorV4();
+        structHash = keccak256(abi.encode(
+            PERMIT_TYPEHASH, 
+            owner,
+            value,
+            keccak256(bytes(_testString)),
+            deadline, 
+            nonce[owner]
+        ));
+        messageHash = _hashTypedDataV4(structHash);
+    }
+
     /** ========== event ========== */
-    event PermittedWithValue(address signer, uint256 value, uint256 deadline);
+    event PermittedWithValue(address signer, uint256 value, string testString, uint256 deadline);
     event PermittedWithHashedMessage(address signer);
 }
