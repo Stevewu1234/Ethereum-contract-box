269e1d6aff75a8ed78c9219c750cdae49f937504
Stevewu1234 <plgawu@163.com>
2022-08-02 22:20:35 +0800

    update import

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,9 +6,8 @@ import '@nomiclabs/hardhat-waffle';
 import '@typechain/hardhat';
 import 'hardhat-gas-reporter';
 import 'solidity-coverage';
-import './tasks/deploy.ts';
 import './tasks/chainCheck.ts';
-import './scripts/transactionChecker/transactionChecker.ts';
+import './scripts/transactionChecker.ts';
 
 dotenv.config();
 
