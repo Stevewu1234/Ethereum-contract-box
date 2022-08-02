5e113987a6ac4313e8274b64672983f57a3dad8c
Stevewu1234 <plgawu@163.com>
2022-08-02 21:11:10 +0800

    initial upgradeable test contract

---
+++ contracts/upgradeTest/ProxyTestAdmin.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
+
+contract ProxyTestAdmin is ProxyAdmin {
+    
+    constructor() {}
+
+    
+
+}
