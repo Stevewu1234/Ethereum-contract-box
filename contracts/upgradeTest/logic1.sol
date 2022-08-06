62828aebb73f8bee03ad550a2742d59829e61bef
Stevewu1234 <plgawu@163.com>
2022-08-06 18:42:41 +0800

    test solidity prettier

--- contracts/upgradeTest/logic1.sol
+++ contracts/upgradeTest/logic1.sol
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract logic1 {
-    
+contract Logic1 {
     uint256 public number1;
+
     string public stringData2;
 
     constructor() {}
-
 }
--- contracts/upgradeTest/logic2.sol
+++ contracts/upgradeTest/logic2.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract logic1 {
-    
+contract Logic2 {
+    uint256 public a;
+
+    constructor(uint256 _a) {
+        a = _a;
+    }
 }
