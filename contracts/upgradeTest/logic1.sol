8dd02231fb96c584400b6e5df115700488f807b2
Stevewu1234 <plgawu@163.com>
2022-08-08 09:57:09 +0800

    update upgradeable deployment test

--- contracts/upgradeTest/logic1.sol
+++ contracts/upgradeTest/logic1.sol
@@ -1,10 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract Logic1 {
-    uint256 public number1;
+import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+
+contract Logic1Upgradeable is Initializable {
+    uint256 public initialNum1;
 
     string public stringData2;
 
-    constructor() {}
+    function logicInit() external initializer {
+        initialNum1 = 1;
+    }
 }
--- test/upgradeTest.ts
+++ test/upgradeTest.ts
@@ -1,35 +1,51 @@
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import { expect } from 'chai';
 import hre from 'hardhat';
-import { Logic1, ProxyTest, ProxyTestAdmin } from '../typechain';
+import {
+  Logic1,
+  Logic1Upgradeable,
+  ProxyTest,
+  ProxyTestAdmin,
+} from '../typechain';
 
 let signer: SignerWithAddress;
 let admin: SignerWithAddress;
 let proxyTestAdmin: ProxyTestAdmin;
 let proxyTest: ProxyTest;
-let logic1: Logic1;
-
+let logic1: Logic1Upgradeable;
 
 beforeEach(async () => {
   [signer, admin] = await hre.ethers.getSigners();
 
-  const logic1Factory = await hre.ethers.getContractFactory('Logic1');
+  const logic1Factory = await hre.ethers.getContractFactory(
+    'Logic1Upgradeable'
+  );
   logic1 = await logic1Factory.deploy();
   await logic1.deployed();
 
-  const proxyTestAdminFactory = await hre.ethers.getContractFactory('ProxyTestAdmin')
+  const proxyTestAdminFactory = await hre.ethers.getContractFactory(
+    'ProxyTestAdmin'
+  );
   proxyTestAdmin = await proxyTestAdminFactory.deploy();
   await proxyTestAdmin.deployed();
 
   const proxyTestFactory = await hre.ethers.getContractFactory('ProxyTest');
-  proxyTest = await proxyTestFactory.deploy(logic1.address, proxyTestAdmin.address, '');
+  proxyTest = await proxyTestFactory.deploy(
+    logic1.address,
+    proxyTestAdmin.address,
+    logic1.interface.encodeFunctionData('logicInit')
+  );
+
   await proxyTest._deployed();
 });
 
-
 describe('initial setting check', () => {
-    it('upgradeable contract deploymentg checking', async () => {
-        expect(await proxyTestAdmin.getProxyAdmin(proxyTest.address)).to.equal(proxyTestAdmin.address);
-        expect(await proxyTestAdmin.getProxyImplementation(proxyTest.address)).to.equal(logic1.address);
-    })
-})
+  it('upgradeable contract deploymentg checking', async () => {
+    expect(await proxyTestAdmin.getProxyAdmin(proxyTest.address)).to.equal(
+      proxyTestAdmin.address
+    );
+    expect(
+      await proxyTestAdmin.getProxyImplementation(proxyTest.address)
+    ).to.equal(logic1.address);
+  });
+});
