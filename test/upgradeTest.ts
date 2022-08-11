ba35dc9306899de92033fbf6b337b3dfc192d56b
Stevewu1234 <plgawu@163.com>
2022-08-11 21:20:41 +0800

    add upgradeable contract test: upgrade to logic2

--- test/upgradeTest.ts
+++ test/upgradeTest.ts
@@ -1,25 +1,20 @@
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import { expect } from 'chai';
+import { BigNumber } from 'ethers';
 import hre from 'hardhat';
-import {
-  Logic1,
-  Logic1Upgradeable,
-  ProxyTest,
-  ProxyTestAdmin,
-} from '../typechain';
+import { Logic1, Logic2, ProxyTest, ProxyTestAdmin } from '../typechain';
 
 let signer: SignerWithAddress;
 let admin: SignerWithAddress;
 let proxyTestAdmin: ProxyTestAdmin;
 let proxyTest: ProxyTest;
-let logic1: Logic1Upgradeable;
+let logic1: Logic1;
+let logic2: Logic2;
 
 beforeEach(async () => {
   [signer, admin] = await hre.ethers.getSigners();
 
-  const logic1Factory = await hre.ethers.getContractFactory(
-    'Logic1Upgradeable'
-  );
+  const logic1Factory = await hre.ethers.getContractFactory('Logic1');
   logic1 = await logic1Factory.deploy();
   await logic1.deployed();
 
@@ -47,5 +42,21 @@ describe('initial setting check', () => {
     expect(
       await proxyTestAdmin.getProxyImplementation(proxyTest.address)
     ).to.equal(logic1.address);
+  });
+});
+
+describe('upgrade to logic2 contract', () => {
+  before('upgrade to logic2', async () => {
+    const logic2Factory = await hre.ethers.getContractFactory('Logic2');
+    logic2 = await logic2Factory.deploy();
+    await logic2.deployed();
+
+    // upgrade to logic2 contract
+    await proxyTestAdmin.upgrade(proxyTest.address, logic2.address);
+  });
+
+  it('call and check logic2 new number', async () => {
+    await logic2.updateNewNumber(BigNumber.from(2));
+    expect(await logic2.number2()).to.equal(2);
   });
 });
