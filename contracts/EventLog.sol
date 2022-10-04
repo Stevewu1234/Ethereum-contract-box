eb8e0ac960064ebfea99c8ff87b6fe1e35b7301a
Stevewu1234 <plgawu@163.com>
2022-10-04 23:30:28 +0800

    add some eveng log template

--- contracts/EventLog.sol
+++ contracts/EventLog.sol
@@ -1,4 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract EeventLog {}
+contract EeventLog {
+    struct Test {
+        uint256 number1;
+    }
+
+    event SimpleTransfer(
+        address indexed sender,
+        address indexed receiver,
+        uint256 amount
+    );
+
+    event StringRecord(address indexed account, string content);
+
+    event NumberRecord(address indexed account, uint256 number);
+
+    event StructRecord(address indexed account, Test test);
+}
